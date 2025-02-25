368c994c3fb554c6793af9f4a44c1b5a2b7de68f
dinaarh <rahmawatidina0404@gmail.com>
2025-02-25 22:16:02 +0700

    Jb 3_Praktikum 6_langkah 3_ modifikasi file UserModel

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -8,4 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class UserModel extends Model
 {
     use HasFactory;
+
+    protected $table = 'm_user'; //mendefinisikan nama tabel model ini
+    protected $primaryKey = 'user_id'; // mendefinisikan primary key dari tabel
 }
