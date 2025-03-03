61a1e20cc5151873ce73abad3db2e45307d98005
dinaarh <rahmawatidina0404@gmail.com>
2025-03-04 06:55:00 +0700

    Jb 4_Praktikum 2.1_langkah 8_Ubah UserController.php_findOr

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,7 +10,9 @@ class UserController extends Controller
 {
     public function index()
     {
-        $user = UserModel::firstWhere('level_id', 1); // ambil data user
+        $user = UserModel::findOr(1, ['username', 'nama'], function() {
+            abort(404);
+        });
         return view('user', ['data' => $user]); //kirim data user ke view
     }
 }
