3d3e77dc271a438380e588b3296ced2d1049e8a3
dinaarh <rahmawatidina0404@gmail.com>
2025-03-04 04:21:05 +0700

    Jb 4_Praktikum 1_langkah 1_tambah $fillable

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -11,4 +11,6 @@ class UserModel extends Model
 
     protected $table = 'm_user'; //mendefinisikan nama tabel model ini
     protected $primaryKey = 'user_id'; // mendefinisikan primary key dari tabel
+
+    protected $fillable = ['level_id', 'username', 'nama', 'password'];
 }
