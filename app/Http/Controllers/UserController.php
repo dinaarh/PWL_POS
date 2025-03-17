7be296f888fda22c85954b8ce09745fc7623fa35
dinaarh <rahmawatidina0404@gmail.com>
2025-03-18 04:49:06 +0700

    Jb 6_Praktikum 1_langkah 7_fungsi create_ajax() UserController_Modal Ajax Tambah User

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -177,6 +177,14 @@ class UserController extends Controller
         }
     }
 
+    public function create_ajax()
+     {
+         $level = LevelModel::select('level_id', 'level_nama')->get();
+ 
+         return view('user.create_ajax')
+         ->with('level', $level);
+     }
+
     public function tambah()
     {
         return view('user_tambah');
