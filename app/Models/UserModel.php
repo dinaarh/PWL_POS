a12fe04d0943015acf9fc1d0cdcfb71ca4cc727f
dinaarh <rahmawatidina0404@gmail.com>
2025-03-25 05:43:39 +0700

    Jb 7_Praktikum 1_Langkah 2_Modifikasi UserModel_$hidden_$casts-Authentication

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-
+use Illuminate\Foundation\Auth\User as Authenticatable; // implementasi class Authenticatable
 
 class UserModel extends Model
 {
@@ -14,7 +14,16 @@ class UserModel extends Model
     protected $table = 'm_user'; //mendefinisikan nama tabel model ini
     protected $primaryKey = 'user_id'; // mendefinisikan primary key dari tabel
 
-    protected $fillable = ['level_id', 'username', 'nama', 'password'];
+    protected $fillable = ['level_id', 'username', 'nama', 'password', 'created_at', 'updated_at'];
+
+    protected $hidden = ['password']; // Jangan ditampilkan saat select
+ 
+    protected $casts = [ 'password' => 'hashed' ]; // casting password agar otomatis di hash
+
+    /**
+      * Relasi ke tabel level
+      */
+
     public function level(): BelongsTo
     {
         return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
