d345338fc09082ce7b6f58da68d59acdc457f71c
dinaarh <rahmawatidina0404@gmail.com>
2025-04-15 04:03:34 +0700

    Jb 8_Praktikum 3_Langkah 3_route proses export pdf

--- routes/web.php
+++ routes/web.php
@@ -155,5 +155,6 @@ Route::middleware(['auth'])->group(function() { // artinya semua route didalam g
         Route::get('/barang/import', [BarangController::class, 'import']); // ajax form upload excel
         Route::post('/barang/import_ajax', [BarangController::class, 'import_ajax']); // ajax import excel
         Route::get('/barang/export_excel', [BarangController::class, 'export_excel']); // menampilkan halaman export barang
+        Route::get('/barang/export_pdf', [BarangController::class, 'export_pdf']); // export pdf
     });
 });
