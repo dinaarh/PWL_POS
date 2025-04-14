160cfaf8e964db06fd3104690650dd6022b08137
dinaarh <rahmawatidina0404@gmail.com>
2025-04-15 00:59:33 +0700

    Jb 8_Praktikum 1_Langkah 9_upload template data

--- resources/views/barang/index.blade.php
+++ resources/views/barang/index.blade.php
@@ -36,7 +36,7 @@
             @if (session('error'))
                 <div class="alert alert-danger">{{ session('error') }}</div>
             @endif
-            <table class="table table-bordered table-striped table-hover table-sm" id="table_barang">
+            <table class="table table-bordered table-striped table-hover table-sm" id="table-barang">
                 <thead>
                     <tr>
                         <th>No</th>
