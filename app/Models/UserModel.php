a7f6420c79e5ca3d8c7286f1e9668ae75a4424a0
dinaarh <rahmawatidina0404@gmail.com>
2025-05-05 19:28:02 +0700

    Jb 10_Praktikum 1_Langkah 7_tambah kode di model UserModel

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -6,10 +6,21 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Foundation\Auth\User as Authenticatable; // implementasi class Authenticatable
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class UserModel extends Authenticatable
+class UserModel extends Authenticatable implements JWTSubject
 {
     use HasFactory;
+
+    public function getJWTIdentifier()
+     {
+         return $this->getKey();
+     }
+ 
+     public function getJWTCustomClaims()
+     {
+         return [];
+     }
 
     protected $table = 'm_user'; //mendefinisikan nama tabel model ini
     protected $primaryKey = 'user_id'; // mendefinisikan primary key dari tabel
